84b2d157f2d0c845912d491a862d3907194e0c4c
not-mBRS <not-mBRS@github.com>
2024-11-01 17:24:15 +0100

    fixed the "size of code" field problem

--- SectionAddition.py
+++ SectionAddition.py
@@ -48,16 +48,14 @@ def get_content(size):
     nops=[]
     while size > 0:
         encoding, count = keys.asm(random.choice(NOP_INSTRUCTIONS))
+        if size == 1:
+            nops+=[0]
+            return bytearray(nops)
         if count > size :
             continue
-        if size == 1:
-            encoding = 0
-            count = 1 
-        size-=count
+        size-=len(bytearray(encoding))
         nops+=encoding
-        print(size)
-    content = bytearray(nops)
-    return content
+    return bytearray(nops)
     
 def inject_sections(input_path, output_path):
     with open(input_path, 'rb') as file:
@@ -66,21 +64,23 @@ def inject_sections(input_path, output_path):
     pe_binary = lief.PE.parse(list(modified_binary))
     
     how_many_bytes = find_target_section(pe_binary)
-    content_to_append = get_content(how_many_bytes) 
+    content_to_append = get_content(how_many_bytes-16) 
     
     section = lief.PE.Section()
-    section.name = ".newsec"
+    section.name = ".data4"
     section.content = content_to_append
-    
+    section.characteristics = pe_binary.get_section(".text").characteristics   
     pe_binary.add_section(section)
-       
+    
+    pe_binary.optional_header.sizeof_code *= 2
+    
     builder = lief.PE.Builder(pe_binary)
     builder.build()
     
     with open(output_path, 'wb') as file:
         file.write(bytearray(builder.get_build()))
     
-input_path = "./calc.exe"     # Original file path
-output_path = "./calc_demetrio.exe"    # Path for the modified file
+input_path = "./calc.exe"     
+output_path = "./calc_demetrio.exe"    
 
 inject_sections(input_path, output_path)
